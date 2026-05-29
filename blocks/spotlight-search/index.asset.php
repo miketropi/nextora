cd101744c263f012dc9bdacdfbe2cf22c6dd1016
dungduc <ducdung196qtr@gmail.com>
2026-05-29 16:56:08 +0700

    update

--- blocks/spotlight-search/index.asset.php
+++ blocks/spotlight-search/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780047714044' ];
-=======
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780024963012' ];
->>>>>>> ee2317f4a6bd2dde03a793deb8d0717e43a76355
+<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780048547517' ];
