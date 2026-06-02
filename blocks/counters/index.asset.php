e5fc165aeb9d865bc38bcb1df8c7891a01836996
dungduc <ducdung196qtr@gmail.com>
2026-06-02 09:33:34 +0700

    update

--- blocks/counters/index.asset.php
+++ blocks/counters/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780366414084' ];
-=======
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780330411200' ];
->>>>>>> ad5acfe39ae458d34105ad55346a248526a1b05d
+<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780367603180' ];
