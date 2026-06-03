8f9ecca0f119eb268f5480e3293ecd83c79a4d49
dungduc <ducdung196qtr@gmail.com>
2026-06-03 09:59:13 +0700

    update

---
+++ patterns/blog-list-carousel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Blog List Carousel
+ * Slug: nextora/blog-list-carousel
+ * Categories: featured
+ * Keywords: blog, list, carousel, posts
+ * Description: A section displaying a list of blog posts with images and titles in a carousel.
+ *
+ * @package Nextora
+ * @subpackage Patterns
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:nextora/blog-list-carousel {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} /--></div>
+<!-- /wp:group -->
