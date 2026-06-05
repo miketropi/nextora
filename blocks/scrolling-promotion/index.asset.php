89e68e115f15e9445a021d809b7136157ed62286
dungduc <ducdung196qtr@gmail.com>
2026-06-05 10:02:49 +0700

    update: logo header, add follow us

--- blocks/scrolling-promotion/index.asset.php
+++ blocks/scrolling-promotion/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780566834139' ];
-=======
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780579754585' ];
->>>>>>> 9200ed2ca23bc370b12e63f011a015fe49e72518
+<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1780627490706' ];
