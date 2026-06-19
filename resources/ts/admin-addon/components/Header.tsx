8254acf909f76d1847baad8e38800b7d3d8fed0f
Mike <mike.beplus@gmail.com>
2026-06-19 09:01:15 +0700

    Update admin extensions

--- resources/ts/admin-addon/components/Header.tsx
+++ resources/ts/admin-addon/components/Header.tsx
@@ -1,4 +1,6 @@
 import { __ } from '@wordpress/i18n';
+import { Mail } from 'lucide';
+import { LucideIcon } from './icons';
 
 export default function Header(): JSX.Element {
 	return (
@@ -11,6 +13,13 @@ export default function Header(): JSX.Element {
 					'Explore child themes, supported extensions, and services available for your Nextora-powered site.',
 					'nextora',
 				)}
+			</p>
+			<p className="nextora-addon-header__contact">
+				<LucideIcon icon={Mail} size={14} className="nextora-addon-header__contact-icon" />
+				{__('Need help? Reach us at', 'nextora')}{' '}
+				<a href="mailto:admin@beplusthemes.com">
+					admin@beplusthemes.com
+				</a>
 			</p>
 		</div>
 	);
