efc4d0a9de21064f126938c5f923bf2df32dd014
dungduc <ducdung196qtr@gmail.com>
2026-06-22 16:58:30 +0700

    update: rebuild

--- assets/js/editor.asset.php
+++ assets/js/editor.asset.php
@@ -1,4 +1,4 @@
 <?php
 
 declare(strict_types=1);
-return array( 'dependencies' => array( 'wp-rich-text', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-data', 'wp-hooks', 'wp-compose' ), 'version' => '1781688941553' );
+return array( 'dependencies' => array( 'wp-rich-text', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-data', 'wp-hooks', 'wp-compose' ), 'version' => '1782122279127' );
