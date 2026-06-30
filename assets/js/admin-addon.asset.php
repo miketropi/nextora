f3ed675a6a26b39b526b45b146eafc4310a0d9f0
dungduc <ducdung196qtr@gmail.com>
2026-06-30 09:41:13 +0700

    fix: resolve merge conflicts

--- assets/js/admin-addon.asset.php
+++ assets/js/admin-addon.asset.php
@@ -1,4 +1,4 @@
 <?php
 
 declare(strict_types=1);
-return array( 'dependencies' => array( 'wp-components', 'wp-element', 'wp-i18n', 'wp-api-fetch' ), 'version' => '1782786306496' );
+return array( 'dependencies' => array( 'wp-components', 'wp-element', 'wp-i18n', 'wp-api-fetch' ), 'version' => '1782786337846' );
