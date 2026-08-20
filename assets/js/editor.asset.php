d3ae8c063b56adee787646a3f2b569f6bb8bc042
thangtienql <thangtienql@gmail.com>
2026-08-20 15:57:42 +0700

    merge from develop

--- assets/js/editor.asset.php
+++ assets/js/editor.asset.php
@@ -1,8 +1,4 @@
 <?php
 
 declare(strict_types=1);
-<<<<<<< HEAD
-return array( 'dependencies' => array( 'wp-rich-text', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-data', 'wp-hooks', 'wp-compose' ), 'version' => '1787048410643' );
-=======
-return array( 'dependencies' => array( 'wp-rich-text', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-data', 'wp-hooks', 'wp-compose' ), 'version' => '1787127605970' );
->>>>>>> c220ca7b8a8d699a7f73f4b66f2c49c503fc7d7e
+return array( 'dependencies' => array( 'wp-rich-text', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-data', 'wp-hooks', 'wp-compose' ), 'version' => '1787216158299' );
--- blocks/plugin-notice/index.asset.php
+++ blocks/plugin-notice/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1787048410872' ];
-=======
-<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1787130154990' ];
->>>>>>> c220ca7b8a8d699a7f73f4b66f2c49c503fc7d7e
+<?php return [ 'dependencies' => [ 'wp-blocks', 'wp-block-editor', 'wp-server-side-render', 'wp-components', 'wp-element', 'wp-i18n', 'wp-hooks', 'wp-data', 'wp-core-data', 'wp-compose', 'wp-primitives', 'wp-blob', 'wp-notices', 'wp-plugins' ], 'version' => '1787216158524' ];
