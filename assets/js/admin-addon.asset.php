c7f3aef074e48d421669247587a90017a006fa20
Mike <mike.beplus@gmail.com>
2026-08-21 16:46:50 +0700

    build: update generated asset php metadata files

--- assets/js/admin-addon.asset.php
+++ assets/js/admin-addon.asset.php
@@ -1,4 +1,4 @@
 <?php
 
 declare(strict_types=1);
-return array( 'dependencies' => array( 'wp-components', 'wp-element', 'wp-i18n', 'wp-api-fetch' ), 'version' => '1787301922038' );
+return array( 'dependencies' => array( 'wp-components', 'wp-element', 'wp-i18n', 'wp-api-fetch' ), 'version' => '1787305448898' );
