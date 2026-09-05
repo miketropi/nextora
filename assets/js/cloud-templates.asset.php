9e47a2d2962e02cbabd22297f2f15a87086297af
Mike <mike.beplus@gmail.com>
2026-09-05 15:45:30 +0700

    Fix issue cloud template modal scroll

--- assets/js/cloud-templates.asset.php
+++ assets/js/cloud-templates.asset.php
@@ -1,4 +1,4 @@
 <?php
 
 declare(strict_types=1);
-return array( 'dependencies' => array( 'wp-components', 'wp-element', 'wp-i18n', 'wp-api-fetch', 'wp-hooks' ), 'version' => '1787823438196' );
+return array( 'dependencies' => array( 'wp-components', 'wp-element', 'wp-i18n', 'wp-api-fetch', 'wp-hooks' ), 'version' => '1788597802699' );
