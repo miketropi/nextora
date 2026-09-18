537512ab2a62d539fe64661dbd8001220c876133
dungduc <ducdung196qtr@gmail.com>
2026-09-18 16:11:49 +0700

    rebuild block

--- assets/js/editor.asset.php
+++ assets/js/editor.asset.php
@@ -1,4 +1,4 @@
 <?php
 
 declare(strict_types=1);
-return array( 'dependencies' => array( 'wp-rich-text', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-data', 'wp-hooks', 'wp-compose', 'wp-plugins', 'wp-edit-post', 'wp-api-fetch' ), 'version' => '1789617721046' );
+return array( 'dependencies' => array( 'wp-rich-text', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-data', 'wp-hooks', 'wp-compose', 'wp-plugins', 'wp-edit-post', 'wp-api-fetch' ), 'version' => '1789713898962' );
